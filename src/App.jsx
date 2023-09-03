f5ea205da75c24531a829497152e4caedcd33294
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-09-03 12:38:53 +0100

    :construction: Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Home from './components/Home'
+
+const App = () => {
+  return(
+    <>
+      <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import "./home.css"
+
+// Generating current date for header
+const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = [
+  'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+]
+const currentDate = new Date()
+const dayIndex = currentDate.getDay()
+const day = dayNames[dayIndex]
+const monthIndex = currentDate.getMonth()
+const month = months[monthIndex]
+const date = currentDate.getDate()
+console.log(day, month, date)
+
+const Home = () => {
+  return (
+    <section className="main-section">
+      <div className="todo">
+        <Nav />
+        <div className="control">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <div className="input-form">
+          <Form />
+        </div>
+      </div>
+      
+    </section>
+  )
+}
+
+const Nav = () => {
+  return(
+    <>
+      <header>
+        <nav>
+          <h1><span>{date}</span> <span>{month}</span>, <span>{day}</span></h1>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+const Card = (props) => {
+  return(
+    <div className="task-card">
+      <div className="task">
+        <h1>Title</h1>
+        <p>Description of task</p>
+      </div>
+      <div className="due-date">
+        <p>12, Sept</p>
+      </div>
+    </div>
+  )
+}
+
+const Form = () => {
+  return (
+    <>
+      <div className="form-main">
+        <input type="text" name="title" placeholder="Title" />
+        <input type="text" name="description" placeholder="Description" />
+      </div>
+      <div className="btn-container">
+        <button className="btn">Create</button>
+      </div>
+    </>
+  )
+}
+
+export default Home

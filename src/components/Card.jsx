6316f20fad0d8597736bf6b7780d9b3968414dd0
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-09-03 13:56:58 +0100

    :sparkles: Added delete button and due date selector

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import Bin from "../assets/bin.svg"
+
+const del = () => {
+  console.log("Delete Button")
+}
 
 const Card = (props) => {
   return(
     <div className="task-card">
       <div className="task">
-        <h1>Title</h1>
-        <p>Coming Soon. I'll soon be done with exams sir</p>
-      </div>
-      <div className="due-date">
-        <p>12, Sept</p>
+        <div className="task-header">
+          <h1>Title</h1>
+          <p>12, Sept</p>
+        </div>
+        <div className="details">
+          <p>Coming Soon. I'll soon be done with exams sir</p>
+          <img src={Bin} className="bin" alt="bucket icon" onClick={del} />
+        </div>
       </div>
     </div>
   )
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,6 +6,10 @@ const Form = () => {
       <div className="form-main">
         <input type="text" name="title" placeholder="Title" />
         <input type="text" name="description" placeholder="Description" />
+        <div className="due-date">
+          {/* <label for="birthday">Due Date</label> */}
+          <input type="date" id="due-date" name="due date" />
+        </div>
       </div>
       <div className="btn-container">
         <button className="btn">Create</button>

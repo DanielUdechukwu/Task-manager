396aa35aff3dd3b8b71545a39b259a91b70f26e6
Daniel Udechukwu <danieludechukwu117@gmail.com>
2023-09-03 12:54:59 +0100

    :lipstick: Updated UI

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -51,7 +51,7 @@ const Card = (props) => {
     <div className="task-card">
       <div className="task">
         <h1>Title</h1>
-        <p>Description of task</p>
+        <p>Coming Soon. I'll soon be done with exams sir</p>
       </div>
       <div className="due-date">
         <p>12, Sept</p>
